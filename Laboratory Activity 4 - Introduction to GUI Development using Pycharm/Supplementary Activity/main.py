4209cdd159f42c21fecec2d5e381468ca2e9b1c8
Frederick Masangkay <qfdmasangkay@tip.edu.ph>
2024-10-14 09:57:14 +0800

    Create main.py

---
+++ Laboratory Activity 4 - Introduction to GUI Development using Pycharm/Supplementary Activity/main.py
@@ -0,0 +1,8 @@
+from registration import myWindow
+from tkinter import *
+
+window = Tk()
+MyWin = myWindow(window)
+window.geometry("400x300+700+300")
+window.title("Registration Form")
+window.mainloop()
